ed2c7cad1327016c83f36a6be552037fc5d0c06d
Pablo Hurtado Gonzalo <pablohurtadohg@gmail.com>
2022-07-21 14:34:26 +0200

    Prueba

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 const app = <img src='/logo.svg' />;
+console.log(app);
 const container = document.getElementById('root');
 
 ReactDOM.render(app, container);
