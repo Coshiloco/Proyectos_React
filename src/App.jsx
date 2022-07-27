c2cbdb2c5eb62afdf2f8931638d851535f25184c
Pablo Hurtado Gonzalo <pablohurtadohg@gmail.com>
2022-07-27 11:58:56 +0200

    Parametro active

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,17 @@ import UserList from './components/UsersList';
 const USERS = [
 	{
 		name: 'Pablo Hurtado Gonzalo',
+		activo: true,
 		role: 'estudiante'
 	},
 	{
 		name: 'Bauty',
+		activo: true,
 		role: 'estudiante'
 	},
 	{
 		name: 'Ninvel',
+		activo: false,
 		role: 'estudiante'
 	}
 ];
--- src/components/UserRow.jsx
+++ src/components/UserRow.jsx
@@ -1,8 +1,11 @@
 import style from './UserRow.module.css';
-const UserRow = ({ name, role }) => (
+const UserRow = ({ name, active, role }) => (
 	<div className={style.user}>
 		<div className={style.name}>
 			<span>{name}</span>
+		</div>
+		<div className={style.active}>
+			<span>{active}</span>
 		</div>
 		<div className={style.role}>
 			<span>{role}</span>
