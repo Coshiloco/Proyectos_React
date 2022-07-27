135dc6544bfcb1bf091d081fdb02e297a9dac3ce
Pablo Hurtado Gonzalo <pablohurtadohg@gmail.com>
2022-07-27 12:45:42 +0200

    Renderizado COndicional

--- src/components/UserRow.jsx
+++ src/components/UserRow.jsx
@@ -1,16 +1,21 @@
 import style from './UserRow.module.css';
-const UserRow = ({ name, active, role }) => (
-	<div className={style.user}>
-		<div className={style.name}>
-			<span>{name}</span>
+const UserRow = ({ name, active, role }) => {
+	const activeClassname = active ? style.active : style.inactive;
+	return (
+		<div className={style.user}>
+			<div className={style.name}>
+				<span>{name}</span>
+			</div>
+			<div className={style.status}>
+				<span className={activeClassname}>
+					{active ? 'Activo' : 'Inactivo'}
+				</span>
+			</div>
+			<div className={style.role}>
+				<span>{role}</span>
+			</div>
 		</div>
-		<div className={style.active}>
-			<span>{active}</span>
-		</div>
-		<div className={style.role}>
-			<span>{role}</span>
-		</div>
-	</div>
-);
+	);
+};
 
 export default UserRow;
