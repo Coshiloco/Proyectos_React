2c456ced540d50acb1817064dae66fea57e3cc27
Pablo Hurtado Gonzalo <pablohurtadohg@gmail.com>
2022-07-28 00:47:25 +0200

    alternativa de usuarios o no

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -2,9 +2,12 @@ import style from './UserLIst.module.css';
 import UserRow from './UserRow';
 
 const UserList = ({ users, children }) => {
-	const usersRendered = users.map(user => (
-		<UserRow key={user.name} {...user} />
-	));
+	const usersRendered =
+		users.length > 0 ? (
+			users.map(user => <UserRow key={user.name} {...user} />)
+		) : (
+			<p>No hay USuarios</p>
+		);
 	return (
 		<div className={style.list}>
 			{children}
