eea36b70b892cc27fcbaf3a6f042e33ac31d16ff
Pablo Hurtado Gonzalo <pablohurtadohg@gmail.com>
2022-07-28 14:19:07 +0200

    Cambios de reflejo

--- src/components/UserRole.jsx
+++ src/components/UserRole.jsx
@@ -1,12 +1,14 @@
 import style from './UserRole.module.css';
 
-const UserRole = ({ role }) => {
+
 	//  Posibles tilos de lo que viene siendo los roles
 	const ROLES_STYLES = {
 		teacher: ['Profesor', style.teacher],
 		student: ['Alumno', style.student],
 		other: ['Otro', style.other]
 	};
+
+const UserRole = ({ role }) => {
 	// O alternativa de roll de otro destructuring
 	const [roleName, roleClassname] = ROLES_STYLES[role] || ROLES_STYLES.other;
 
