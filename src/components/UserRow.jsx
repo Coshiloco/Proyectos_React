77282d5ce4e536b7cf1724187eb6867e2d41585a
Pablo Hurtado Gonzalo <pablohurtadohg@gmail.com>
2022-07-28 08:10:45 +0200

    UseState UserRow boton

--- src/components/UserRow.jsx
+++ src/components/UserRow.jsx
@@ -1,17 +1,31 @@
+import { useState } from 'react';
 import UserRole from './UserRole';
 import style from './UserRow.module.css';
 import UserStatus from './UserStatus';
+
 const UserRow = ({ name, active, role }) => {
+	// Almacenamos el estado del usuario al momento de su creacion
+	// Para pasar el estado de activo o inactivo al ReactDOm
+	const [activeState, setActiveState] = useState(active);
 	return (
 		<div className={style.user}>
 			<div className={style.name}>
 				<span>{name}</span>
 			</div>
 			<div className={style.status}>
-				<UserStatus active={active}></UserStatus>
+				<UserStatus active={activeState}></UserStatus>
 			</div>
 			<div className={style.role}>
 				<UserRole role={role}></UserRole>
+			</div>
+			<div className={style.action}>
+				<button
+					onClick={() => {
+						setActiveState(!activeState);
+					}}
+				>
+					{activeState ? 'Desactivar' : 'Activar'}
+				</button>
 			</div>
 		</div>
 	);
