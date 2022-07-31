b6dc77b7601a22cc65c1dd20158aae147e3a8b91
Pablo Hurtado Gonzalo <pablohurtadohg@gmail.com>
2022-07-31 12:25:33 +0200

    EeventBubbling

---
+++ src/components/EventBubblingCapturingPropagation.jsx
@@ -0,0 +1,21 @@
+import './styles/index.css';
+
+const AppEventBubblingCapturingStopPropagation = () => {
+	<div
+		onClick={ev => {
+			console.log('DIV - Target', ev.target);
+			console.log('DIV - CurrentTarget', ev.currentTarget);
+		}}
+	>
+		<button
+		onClick={ev => {
+			console.log('BTN - Target', ev.target);
+			console.log('BTN - CurrentTarget', ev.currentTarget);
+		}}
+		>
+		React
+		</button>
+	</div>;
+};
+
+export default AppEventBubblingCapturingStopPropagation;
