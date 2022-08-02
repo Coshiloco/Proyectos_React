813b22efd8b965f42914601864f4bd3f858a3539
Pablo Hurtado Gonzalo <pablohurtadohg@gmail.com>
2022-08-02 21:40:33 +0200

    Formularios y mas filtros

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -4,33 +4,42 @@ import UserRow from './UserRow';
 
 const UserList = ({ users }) => {
 	const [search, setSearch] = useState('');
-	const usersRendered = filterAndRenderUsers(users, search);
+	const usersFiltered = filterUsersByName(users, search);
+	const usersRendered = renderUsers(usersFiltered);
 	return (
 		<div className={style.list}>
 			<h1>Listado de usuarios</h1>
+			<form className={style.form}>
 			<input
 				type='text'
-				name='search'
 				value={search}
 				onChange={ev => setSearch(ev.target.value)}
 			></input>
+			<div className={style.active}>
+				<input type='checkbox'></input>
+				<span>Solo activos</span>
+			</div>
+			<select>
+				<option value={0}>Por defecto</option>
+				<option value={1}>Por nombre</option>
+			</select>
+			</form>
 			{usersRendered}
 		</div>
 	);
 };
 
-const filterAndRenderUsers = (users, search) => {
-	const normalizedSearch = search.toLowerCase();
-	const usersFiltered = search
-		? users.filter(user => user.name.toLowerCase().startsWith(normalizedSearch))
-		: users;
-	const usersRendered =
-		usersFiltered.length > 0 ? (
-			usersFiltered.map(user => <UserRow key={user.name} {...user} />)
-		) : (
-			<p>No hay USuarios</p>
-		);
-	return usersRendered;
+const filterUsersByName = (users, search) => {
+	if (!search) return users;
+	const lowerCaseSearch = search.toLowerCase();
+	return users.filter(user =>
+		user.name.toLowerCase().startsWith(lowerCaseSearch)
+	);
+};
+
+const renderUsers = users => {
+	if (users.length <= 0) return <p>No hay usuarios</p>;
+	return users.map(user => <UserRow key={user.name} {...user} />);
 };
 
 export default UserList;
