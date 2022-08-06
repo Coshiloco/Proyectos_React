a0d9a45d57ba0deefbcea1c734cf5119a0ac63d6
Pablo Hurtado Gonzalo <pablohurtadohg@gmail.com>
2022-08-06 11:25:01 +0200

    Id fundamental

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,25 @@ import UserList from './components/UsersList';
 
 const USERS = [
 	{
+		id: 0,
 		name: 'Pablo Hurtado Gonzalo',
 		active: true,
 		role: 'teacher'
 	},
 	{
+		id: 1,
 		name: 'Bauty',
 		active: true,
 		role: 'student'
 	},
 	{
+		id: 2,
 		name: 'Ninvel',
 		active: false,
 		role: 'other'
 	},
 	{
+		id: 3,
 		name: 'Fabio',
 		active: false,
 		role: 'teacher'
